14fec30206486bd691dad774ad0b226cc7f30d15
Alvaro <alvaro@fractal.ai>
2015-08-20 10:07:18 -0500

    Función average completa, agregué funcion auxiliar sum-lst que calcula la suma de los elementos de una lista

--- Practicas/practica1/Practica1.rkt
+++ Practicas/practica1/Practica1.rkt
@@ -1,11 +1,15 @@
 #lang plai
 
+;EJERCICIO 1: Toma dos números enteros positivos z y w y regresa el numero que se
+;obtiene de elevar el numero z a la potencia w.
+
 (define (pow z w)
   (cond 
     [(= w 0) 1]
     [(= w 1) z]
     [else (* (* z z) (pow z (- w 2)))]))
 
+;Calcula la longitud de una lista
 (define (long lst)
   (if (empty? lst)
     0
@@ -17,14 +21,18 @@
 (test (pow 8 6) 262144)
 (test (pow 10 5) 100000)
 
-(test (long '()) 0)
-(test (long '(1 2 3)) 3)
+;Suma los elementos de una lista
+(define (sum-lst lst)
+  (if (empty? lst)
+    0
+    (+ (first lst) (sum-lst (cdr lst)))))
 
+;Calcula el promedio de una lista
 (define (average a-lst)
   (if (empty? a-lst)
     0
-    (/ (+ (first a_lst) (average (cdr a_lst))) (long a-lst))))
+    (/ (sum-lst a-lst) (long a-lst))))
 
 (test (average '(5)) 5)
 (test (average '(3 2 6 2 1 7 2 1)) 3)
-(test (average '((10 7 13)) 10))
+(test (average '(10 7 13)) 10)
