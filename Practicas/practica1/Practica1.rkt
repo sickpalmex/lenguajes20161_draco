6e9d45984f41bdb8ae32f5b8ccbe2da3640c3df5
Alvaro <alvaro@fractal.ai>
2015-08-20 18:27:09 -0500

    Agregué función zip (funcionando)

--- Practicas/practica1/Practica1.rkt
+++ Practicas/practica1/Practica1.rkt
@@ -1,19 +1,54 @@
 #lang plai
 
-;EJERCICIO 1: Toma dos números enteros positivos z y w y regresa el numero que se
-;obtiene de elevar el numero z a la potencia w.
+; ========== FUNCIONES AUXILIARES ==========
 
-(define (pow z w)
-  (cond 
-    [(= w 0) 1]
-    [(= w 1) z]
-    [else (* (* z z) (pow z (- w 2)))]))
-
-;Calcula la longitud de una lista
+; Calcula la longitud de una lista
 (define (long lst)
   (if (empty? lst)
     0
     (+ 1 (long (cdr lst)))))
+
+; Suma los elementos de una lista
+(define (sum-lst lst)
+  (if (empty? lst)
+    0
+    (+ (first lst) (sum-lst (cdr lst)))))
+
+; Dadas dos listas, devuelve una lista de dos elementos con la cabeza de cada una de las listas recibidas
+(define (concat-head a-lst b-lst)
+  (if (or (empty? a-lst)(empty? b-lst))
+    '()
+    (list (first a-lst)(first b-lst))))
+
+; ========== PRACTICA 1 ==========
+
+; EJERCICIO 1: Toma dos números enteros positivos z y w y regresa el numero que se obtiene de elevar el numero z a la potencia w.
+
+(define (pow z w)
+  (cond
+    [(= w 0) 1]
+    [(= w 1) z]
+    [else (* (* z z) (pow z (- w 2)))]))
+
+; EJERCICIO 2: Dado una lista no vac´ıa de n´umeros, regresar el promedio de esta
+
+(define (average a-lst)
+  (if (empty? a-lst)
+    0
+    (/ (sum-lst a-lst) (long a-lst))))
+
+; EJERCICIO 3: Dado un numero entero positivo, regresa una lista con los números primos contenidos entre 2 y el numero entero dado
+
+; EJERCICIO 4: zip
+
+(define (zip a-lst b-lst)
+  (cond
+    [(or (empty? a-lst) (empty? b-lst)) '()]
+    [else (cons (concat-head a-lst b-lst)(zip (cdr a-lst)(cdr b-lst)))]))
+
+; ========== TESTS ==========
+
+; Tests Ejercicio 1
 
 (test (pow 2000 0) 1)
 (test (pow 2000 1) 2000)
@@ -21,18 +56,16 @@
 (test (pow 8 6) 262144)
 (test (pow 10 5) 100000)
 
-;Suma los elementos de una lista
-(define (sum-lst lst)
-  (if (empty? lst)
-    0
-    (+ (first lst) (sum-lst (cdr lst)))))
-
-;Calcula el promedio de una lista
-(define (average a-lst)
-  (if (empty? a-lst)
-    0
-    (/ (sum-lst a-lst) (long a-lst))))
+; Tests Ejercicio 2
 
 (test (average '(5)) 5)
 (test (average '(3 2 6 2 1 7 2 1)) 3)
 (test (average '(10 7 13)) 10)
+
+; Tests Ejercicio 4
+
+(test (zip '(1 2) '(3 4)) '((1 3) (2 4)))
+(test (zip '(1 2 3) '()) '())
+(test (zip '() '(4 5 6)) '())
+(test (zip '(8 9) '(3 2 1 4)) '((8 3) (9 2)))
+(test (zip '(8 9 1 2) '(3 4)) '((8 3) (9 4)))
